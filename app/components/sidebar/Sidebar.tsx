22bd7f06ac58974907214fe96c40bc5cf0b66609
henrytrieu88@gmail.com <henrytrieu88@gmail.com>
2024-01-19 22:28:36 -0800

    feat: Add DesktopSidebar and DesktopItem components
    - Implement DesktopSidebar component with responsive design.
    
    - Add DesktopItem component for conversations/users/logout items on sidebar for desktop view.
    
    - Integrate DesktopSidebar into the Sidebar component.

--- app/components/sidebar/Sidebar.tsx
+++ app/components/sidebar/Sidebar.tsx
@@ -1,6 +1,9 @@
+import DesktopSidebar from './DesktopSidebar';
+
 async function Sidebar({ children }: { children: React.ReactNode }) {
   return (
     <div className="h-full">
+      <DesktopSidebar />
       <main className="lg:pl-20 h-full">{children}</main>
     </div>
   );
