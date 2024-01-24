eb124e03f9717342b46c78247febb9815c735d51
henrytrieu88@gmail.com <henrytrieu88@gmail.com>
2024-01-23 19:58:20 -0800

    style: Reformat AuthSocialButton and Button components.
    Reformat these two button components to stay consistent with codebase style.

--- app/(site)/components/AuthSocialButton.tsx
+++ app/(site)/components/AuthSocialButton.tsx
@@ -5,29 +5,29 @@ interface AuthSocialButtonProps {
   onClick: () => void;
 }
 
-const baseButtonClasses = `
-  inline-flex
-  w-full
-  justify-center
-  rounded-md
-  bg-white
-  px-4
-  py-2
-  text-gray-500
-  shadow-sm
-  ring-1
-  ring-inset
-  ring-gray-300
-  hover:bg-gray-50
-  focus:outline-offset-0
-`;
-
 const AuthSocialButton: React.FC<AuthSocialButtonProps> = ({
   icon: Icon,
   onClick,
 }) => {
+  const socialButtonClasses = `
+    inline-flex
+    w-full
+    justify-center
+    rounded-md
+    bg-white
+    px-4
+    py-2
+    text-gray-500
+    shadow-sm
+    ring-1
+    ring-inset
+    ring-gray-300
+    hover:bg-gray-50
+    focus:outline-offset-0
+  `;
+
   return (
-    <button type="button" onClick={onClick} className={baseButtonClasses}>
+    <button type="button" onClick={onClick} className={socialButtonClasses}>
       <Icon />
     </button>
   );
--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -12,19 +12,6 @@ interface ButtonProps {
   disabled?: boolean;
 }
 
-const baseButtonClasses = `
-  flex
-  justify-center
-  rounded-md
-  px-3
-  py-2
-  text-sm
-  font-semibold
-  focus-visible:outline
-  focus-visible:outline-2
-  focus-visible:outline-offset-2
-`;
-
 const Button: React.FC<ButtonProps> = ({
   type,
   fullWidth,
@@ -34,22 +21,33 @@ const Button: React.FC<ButtonProps> = ({
   danger,
   disabled,
 }) => {
+  const buttonClasses = clsx(
+    `
+    flex
+    justify-center
+    rounded-md
+    px-3
+    py-2
+    text-sm
+    font-semibold
+    focus-visible:outline
+    focus-visible:outline-2
+    focus-visible:outline-offset-2`,
+    disabled && 'opacity-50 cursor-default',
+    fullWidth && 'w-full',
+    secondary ? 'text-gray-900' : 'text-white',
+    danger && 'bg-rose-500 hover:bg-rose-600 focus-visible:outline-rose-600',
+    !secondary &&
+      !danger &&
+      'bg-sky-500 hover:bg-sky-600 focus-visible:outline-sky-600'
+  );
+
   return (
     <button
       onClick={onClick}
       type={type}
       disabled={disabled}
-      className={clsx(
-        baseButtonClasses,
-        disabled && 'opacity-50 cursor-default',
-        fullWidth && 'w-full',
-        secondary ? 'text-gray-900' : 'text-white',
-        danger &&
-          'bg-rose-500 hover:bg-rose-600 focus-visible:outline-rose-600',
-        !secondary &&
-          !danger &&
-          'bg-sky-500 hover:bg-sky-600 focus-visible:outline-sky-600',
-      )}
+      className={buttonClasses}
     >
       {children}
     </button>
